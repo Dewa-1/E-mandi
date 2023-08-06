e394af7294de81d4bb1154f869f453e09163e259
Dewa-1 <devashishverma386@gmail.com>
2023-08-06 21:29:48 +0530

    First Commit

---
+++ src/components/FooterIt.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-footer dark padless>
+    <v-card flat tile class="black white--text py-12 px-5" width="100%">
+      <v-row>
+        <v-col cols="12" xs="12" sm="6" md="4" class="pa-16">
+          <v-toolbar color="transparent" flat>
+            <v-badge color="#D5F0DB" dot>
+              <v-img src="1.png" contain width="30"></v-img>
+            </v-badge>
+
+            <v-toolbar-title class="ml-4">
+              <span class="green--text">e</span><strong>Grocery</strong>
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card-text class="pt-0">
+            <v-btn
+              v-for="icon in icons"
+              :key="icon"
+              class="mx-1 white--text"
+              icon
+              fab
+              outlined
+              small
+            >
+              <v-icon size="20px">
+                {{ icon }}
+              </v-icon>
+            </v-btn>
+          </v-card-text>
+        </v-col>
+      
+        <v-col cols="12" xs="12" sm="6" md="4">
+          <v-card-text class="white--text pt-0">
+            <h3>Contact</h3>
+          </v-card-text>
+          <v-card-text class="grey--text"> Bani Banthara, Lucknow </v-card-text>
+          <v-card-text class="grey--text mt-n4">
+            Near G-rest Hotel, Bani - 226401
+          </v-card-text>
+          <v-card-text class="grey--text mt-n4"> devashishverma386@gmail.com </v-card-text>
+          <v-card-text class="grey--text mt-n4"> -7607235537 </v-card-text>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: [
+    //   <a href = " "><i class = "fab fa-apple"></i></a>,
+      <a href = "devashishverma386@gmail.com"><i class = "fab fa-google-plus-g"></i></a>,
+      <a href = "https://twitter.com/Dave10924648"><i class = "fab fa-twitter"></i></a>,
+      <a href = "https://www.facebook.com/devill.rock.948"><i class = "fab fa-facebook-f"></i></a>
+    ],
+  }),
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/NavbarIt.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app-bar app color="white" flat>
+    <v-badge color="#D5FODB" dot>
+        <v-img src="1.png" contain width="30"></v-img>
+    </v-badge>
+    <v-toolbar-title class="ml-4">
+        <span class="green-text">e-</span><strong>Mandi</strong>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+      <v-btn
+                      color="green"
+                      dark
+                      x-large
+                      class="py-7 d-none d-sm-none d-md-flex"
+                    >
+                      Login
+                    </v-btn>
+    <v-badge color="#FF6D59" overlap content="2" class="mr-2 mt-1">
+      <v-avatar color="#FFF0EE" size="40">
+        <v-icon x-small color="#FF6D59">far fa-heart</v-icon>
+      </v-avatar>
+    </v-badge>
+    <v-badge color="#41AB55" overlap content="3" class="mr-2 mt-1">
+        <v-avatar color="#ECF7EE" size="40">
+            <v-icon x-small color="#41AB55">fas fa-shopping-cart</v-icon>
+        </v-avatar>
+    </v-badge>
+    <span class="grey--text d-none d-sm-flex">Hello,<strong></strong>Tony</span>
+    <v-avatar size="30" class="ml-2">
+        <v-img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"></v-img>
+    </v-avatar>
+    <v-btn icon dark>
+        <v-icon x-small color="#878A94">fas fa-chevron-down</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+name:'NavbarIt',
+}
+</script>
+
+<style>
+.v-toolbar_title{
+    font-size: 1rem !important;
+}
+ul{
+    display: flex;
+    list-style: none;
+    
+}
+ul, li{
+    margin: 73px;
+    
+}
+</style>
